7ea742bc1b5d05ec33eb7b3cef03979db4130947
Yato GM <Yato GM>
2017-07-10 01:55:35 +0700

    init news.model.js

---
+++ src/server/model/news.model.js
@@ -0,0 +1,17 @@
+// _id: ObjectId,
+// 			title: nông sản sạch,
+// 			content: ,
+// 			description: '',
+// 			image: [
+// 				{
+// 					_id:
+// 					url: 
+// 				}
+// 			],
+// 			author: "",
+// 			SpiderId: ---,
+// 			URLId: ----,
+// 			TypeId: ---,
+// 			URL: ---,
+// 			CreateDays: ---,
+// 			UpdateDays: ---,
