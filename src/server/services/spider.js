1b27fee900d0b66a0b27564b798492fb30f9e142
tranleanhthe <tranleanhthe@gmail.com>
2017-07-31 01:23:41 +0700

    update spider

--- src/server/services/spider.js
+++ src/server/services/spider.js
@@ -11,8 +11,16 @@ module.exports = {
   spiderTinNongNghiep_path: spiderTinNongNghiep_path,
   spiderTinNongNghiep_updatePath: spiderTinNongNghiep_updatePath,
   spiderTinNongNghiep_Url: spiderTinNongNghiep_Url,
-  spiderTinNongNghiep_updateUrl: spiderTinNongNghiep_updateUrl
+  spiderTinNongNghiep_updateUrl: spiderTinNongNghiep_updateUrl,
+
+  spiderNongNghiepVietNam: spiderNongNghiepVietNam,
+  spiderNongNghiepVietNam_path: spiderNongNghiepVietNam_path,
+  spiderNongNghiepVietNam_Url: spiderNongNghiepVietNam_Url,
+  spiderNongNghiepVietNam_updateAll: spiderNongNghiepVietNam_updateAll,
+  spiderNongNghiepVietNam_updateUrl: spiderNongNghiepVietNam_updateUrl,
+  spiderNongNghiepVietNam_updatePath: spiderNongNghiepVietNam_updatePath
 }
+
 
 function spiderTinNongNghiep(urlId, spiderId) {
   console.log(urlId);
@@ -22,6 +30,13 @@ function spiderTinNongNghiep(urlId, spiderId) {
     var disUrl = urlId.hostname + url.namePath;
     getPath_spiderTinNongNghiep(disUrl, spiderId, url.catelogyId);
   });
+}
+
+function spiderNongNghiepVietNam(urlId, spiderId) {
+  urlId.path.forEach(url => {
+    var disUrl = urlId.hostname + url.namePath;
+    getPath_spiderNongNghiepVietNam(disUrl, spiderId, url.catelogoryId);
+  })
 }
 
 function getPath_spiderTinNongNghiep(path, spiderId, catelogyId) {
@@ -70,12 +85,68 @@ function getPath_spiderTinNongNghiep(path, spiderId, catelogyId) {
   return;
 }
 
+function getPath_spiderNongNghiepVietNam(path, spiderId, catelogyId) {
+  if (path === undefined) {
+    return;
+  }
+  request(path, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var $ = cheerio.load(body);
+      var i = 1;
+      $('#main-content > div > div.post-listing.archive-box').each(function () {
+        //#main-content > div.content > div.post-listing > article:nth-child(1)
+        url = ($(this).attr('href'));
+        image = $('#main-content > div > div.post-listing.archive-box > article:nth-child(' + i + ') > div.post-thumbnail > a > img').attr('src');
+        console.log(image);
+        des = $('#main-content > div > div.post-listing.archive-box > article:nth-child(' + i + ') > div.entry > p').text();
+        if (image === undefined) {
+          image = null;
+        } else {
+          image = image.split('-310x165').join('');
+        }
+        var news = new News({
+          originalLink: url,
+          spiderId: spiderId,
+          categoryId: catelogyId,
+          image: image,
+          description: des,
+          active: true
+        });
+        News.findOne({
+          originalLink: news.originalLink
+        }, function (err, New) {
+          if (New === null) {
+            news.save();
+          }
+        });
+        i++;
+      });
+    }
+    gotoPage = $('#tie-next-page > a').attr('href');
+    if (gotoPage === undefined) {
+      return;
+    }
+    getPath_spiderNongNghiepVietNam(gotoPage, spiderId, catelogyId);
+  });
+  return;
+}
+
 function spiderTinNongNghiep_path(urlId, spiderId, catelogyId) {
   console.log(urlId);
   urlId.path.forEach(url => {
     if (url.catelogyId.equals(catelogyId)) {
       var disUrl = urlId.hostname + url.namePath;
       getPath_spiderTinNongNghiep(disUrl, spiderId, url.catelogyId);
+    }
+  });
+}
+
+function spiderNongNghiepVietNam_path(urlId, spiderId, catelogyId) {
+  console.log(urlId);
+  urlId.path.forEach(url => {
+    if (url.catelogyId.equals(catelogyId)) {
+      var disUrl = urlId.hostname + url.namePath;
+      getPath_spiderNongNghiepVietNam(disUrl, spiderId, url.catelogyId);
     }
   });
 }
@@ -95,6 +166,23 @@ function spiderTinNongNghiep_Url(urlId, spiderId, url) {
     return true;
   });
 }
+
+function spiderNongNghiepVietNam_Url(urlId, spiderId, url) {
+  News.findOne({
+    originalLink: url
+  }, function (err, tNews) {
+    if (tNews !== null) {
+      return false;
+    }
+    var upNews = new News({
+      originalLink: url,
+      spiderId: spiderId,
+    });
+    upNews.save();
+    return true;
+  });
+}
+
 
 function spiderTinNongNghiep_updateAll() {
   News.find({}, function (err, news) {
@@ -162,6 +250,71 @@ function spiderTinNongNghiep_updateAll() {
   });
 }
 
+function spiderNongNghiepVietNam_updateAll() {
+  News.find({}, function (err, news) {
+    var page = 0;
+    var lastPage = news.length;
+    async.whilst(function () {
+        return page < lastPage;
+      },
+      function (next) {
+        if (news[page].title === undefined || news[page].title === "") {
+          request(news[page].originalLink, function (err, res, body) {
+            if (!err && res.statusCode === 200) {
+              var $ = cheerio.load(body);
+              async.series({
+                  title: function (callback) {
+                    news[page].title = $('#the-post > div > h1 > span').text();
+                    callback(null, news[page].title);
+                  },
+                  content: function (callback) {
+                    let content = $('#the-post > div > div.entry').html();
+                    let remove = $('#main-content > div.content > article > div > div.entry > div.share-post').html();
+                    // let remove_review_overview = $('#main-content > div.content > article > div > div.entry > div.review-box.review-top.review-stars').html();
+                    callback(null, content.split(remove).join(''));
+                  },
+                  author: function (callback) {
+                    let author = $('#the-post > div > p > span.post-meta-author > a').text();
+                    news[page].author = author;
+                    callback(null, news[page].author);
+                  },
+                  createDate: function (callback) {
+                    var date = new Date();
+                    var dateF = $('#the-post > div > p > span.tie-date').text().split('/');
+                    date.setDate(dateF[0]);
+                    date.setMonth(dateF[1]);
+                    date.setFullYear(dateF[2]);
+                    callback(null, date);
+                  },
+                  updateDate: function (callback) {
+                    callback(null, Date.now());
+                  }
+                },
+                function (err, result) {
+                  news[page].title = result.title;
+                  news[page].content = result.content;
+                  news[page].author = result.author;
+                  news[page].createDate = result.createDate;
+                  news[page].updateDate = result.updateDate;
+                  console.log(news[page].title);
+                  console.log(news[page].createDate);
+                  news[page].save(function (err) {
+                    if (err) {
+                      console.log('error');
+                    }
+                  });
+                });
+            } else {
+              console.log('log die');
+            }
+            page++;
+            next();
+          });
+        }
+      },
+      function (err) {});
+  });
+}
 
 function spiderTinNongNghiep_updatePath(categoryId) {
   News.find({
@@ -194,6 +347,75 @@ function spiderTinNongNghiep_updatePath(categoryId) {
                   createDate: function (callback) {
                     var date = new Date();
                     var dateF = $('#main-content > div.content > article > div > p > span:nth-child(2)').text().split('/');
+                    date.setDate(dateF[0]);
+                    date.setMonth(dateF[1]);
+                    date.setFullYear(dateF[2]);
+                    callback(null, date);
+                  },
+                  updateDate: function (callback) {
+                    callback(null, Date.now());
+                  }
+                },
+                function (err, result) {
+                  news[page].title = result.title;
+                  news[page].content = result.content;
+                  news[page].author = result.author;
+                  news[page].createDate = result.createDate;
+                  news[page].updateDate = result.updateDate;
+                  console.log(news[page].title);
+                  console.log(news[page].createDate);
+                  news[page].save(function (err) {
+                    if (err) {
+                      console.log('error');
+                    }
+                  });
+                });
+            } else {
+              console.log('log die');
+            }
+            page++;
+            next();
+          });
+        }
+      },
+      function (err) {});
+  });
+}
+
+
+function spiderNongNghiepVietNam_updatePath(categoryId) {
+  News.find({
+    categoryId: categoryId
+  }, function (err, news) {
+    var page = 0;
+    var lastPage = news.length;
+    async.whilst(function () {
+        return page < lastPage;
+      },
+      function (next) {
+        if (news[page].title === undefined || news[page].title === "") {
+          request(news[page].originalLink, function (err, res, body) {
+            if (!err && res.statusCode === 200) {
+              var $ = cheerio.load(body);
+              async.series({
+                  title: function (callback) {
+                    news[page].title = $('#the-post > div > h1 > span').text();
+                    callback(null, news[page].title);
+                  },
+                  content: function (callback) {
+                    let content = $('#the-post > div > div.entry').html();
+                    let remove = $('#main-content > div.content > article > div > div.entry > div.share-post').html();
+                    // let remove_review_overview = $('#main-content > div.content > article > div > div.entry > div.review-box.review-top.review-stars').html();
+                    callback(null, content.split(remove).join(''));
+                  },
+                  author: function (callback) {
+                    let author = $('#the-post > div > p > span.post-meta-author > a').text();
+                    news[page].author = author;
+                    callback(null, news[page].author);
+                  },
+                  createDate: function (callback) {
+                    var date = new Date();
+                    var dateF = $('#the-post > div > p > span.tie-date').text().split('/');
                     date.setDate(dateF[0]);
                     date.setMonth(dateF[1]);
                     date.setFullYear(dateF[2]);
@@ -281,6 +503,68 @@ function spiderTinNongNghiep_updateUrl(url) {
         } else {
           console.log('log die');
         }
+      });
+    }
+  });
+}
+
+
+function spiderNongNghiepVietNam_updateUrl(url) {
+  News.findById({
+    _id: url
+  }, function (err, upNews) {
+    if (upNews !== null) {
+      console.log('this is upNews ' + upNews);
+      request(news[page].originalLink, function (err, res, body) {
+        if (!err && res.statusCode === 200) {
+          var $ = cheerio.load(body);
+          async.series({
+              title: function (callback) {
+                news[page].title = $('#the-post > div > h1 > span').text();
+                callback(null, news[page].title);
+              },
+              content: function (callback) {
+                let content = $('#the-post > div > div.entry').html();
+                let remove = $('#main-content > div.content > article > div > div.entry > div.share-post').html();
+                // let remove_review_overview = $('#main-content > div.content > article > div > div.entry > div.review-box.review-top.review-stars').html();
+                callback(null, content.split(remove).join(''));
+              },
+              author: function (callback) {
+                let author = $('#the-post > div > p > span.post-meta-author > a').text();
+                news[page].author = author;
+                callback(null, news[page].author);
+              },
+              createDate: function (callback) {
+                var date = new Date();
+                var dateF = $('#the-post > div > p > span.tie-date').text().split('/');
+                date.setDate(dateF[0]);
+                date.setMonth(dateF[1]);
+                date.setFullYear(dateF[2]);
+                callback(null, date);
+              },
+              updateDate: function (callback) {
+                callback(null, Date.now());
+              }
+            },
+            function (err, result) {
+              news[page].title = result.title;
+              news[page].content = result.content;
+              news[page].author = result.author;
+              news[page].createDate = result.createDate;
+              news[page].updateDate = result.updateDate;
+              console.log(news[page].title);
+              console.log(news[page].createDate);
+              news[page].save(function (err) {
+                if (err) {
+                  console.log('error');
+                }
+              });
+            });
+        } else {
+          console.log('log die');
+        }
+        page++;
+        next();
       });
     }
   });
