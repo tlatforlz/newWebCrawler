23776bede2bb7ef1b1324c07edc3b9f9bf5abe3c
Tran Le Anh The <tranleanhthe@secure.janeto.com>
2017-11-03 16:15:55 +0700

    fix some bugs

--- src/server/app.js
+++ src/server/app.js
@@ -20,7 +20,19 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow  
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent  
+  // to the API (e.g. in case you use sessions)  
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware 
+  next();
+});
 app.use(config.BASE_URL + '/url', require('./routes/url.routes')());
 app.use(config.BASE_URL + '/spider', require('./routes/spider.routes')());
 app.use(config.BASE_URL + '/category', require('./routes/category.routes')());
